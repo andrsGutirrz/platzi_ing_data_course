c8e22f755b3cf625a58c36514aa7cc8c5bb027c0
andrsGutirrz <andres.gutierrez.arcia@gmail.com>
2019-06-15 23:54:56 -0600

    fix scraper

--- web_scrapper/main.py
+++ web_scrapper/main.py
@@ -1,7 +1,7 @@
 import argparse
 import logging
 import re
-from requests.exceptions import HTTPError
+from requests.exceptions import HTTPError, InvalidSchema
 from urllib3.exceptions import MaxRetryError
 from web_scrapper.common import config
 import web_scrapper.news_page_objects as news
@@ -13,20 +13,21 @@ logger = logging.getLogger(__name__)
 is_well_formed_link = re.compile(r'https?://.+/.+$')
 is_root_path = re.compile(r'^/.+$')
 
-def _news_scraper(news_site_uid):
-    host = config()['news_sites'][news_site_uid]['url']
+def _news_scraper(news_site_uid): #news_site_uid: eluniversal
+    host = config()['news_sites'][news_site_uid]['url'] # http://www.eluniversal.com.mx
     logger.info(f'Start scraper for {host}')
     homepage = news.Homepage(news_site_uid, host)
     articles = []
     for link in homepage.article_links:
         article = _fetch_article(news_site_uid, host, link)
 
-    if article:
-        logger.info('ARTICLE FETCHED!!')
-        articles.append(article)
-        print(article.title)
+        if article:
+            logger.info('ARTICLE FETCHED!!')
+            articles.append(article)
+            print(article.title)
 
     print(len(articles))
+
 
 def _fetch_article(news_site_uid, host, link):
     logger.info(f'START FETCHING ARTICLE AT {link}')
@@ -34,8 +35,8 @@ def _fetch_article(news_site_uid, host, link):
     article = None
     try:
         article = news.ArticlePage(news_site_uid, _build_link(host, link))
-    except (HTTPError, MaxRetryError) as e:
-        logger.warning('Error while fetching',exc_info=False)
+    except (HTTPError, MaxRetryError, InvalidSchema) as e:
+        logger.warning(f'Error while fetching: {e}',exc_info=False)
 
     if article and not article.body:
         logger.warning('Invalid article. There is no body')
@@ -43,11 +44,13 @@ def _fetch_article(news_site_uid, host, link):
 
     return article
 
+
 def _build_link(host, link):
-    if is_well_formed_link.match(link['href']):
+    link_spread = link['href']
+    if is_well_formed_link.match(link_spread):
         return link
-    elif is_root_path.match(link['href']):
-        return f'{host}{link}'
+    elif is_root_path.match(link_spread):
+        return f'{host}{link_spread}'
     else:
         f'{host}/{link}'
 
